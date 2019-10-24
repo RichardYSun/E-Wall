518ca08184cf6607c9e290c3a52c6c9b5bbae165
Sunny Lan <sunny.lan@uwaterloo.ca>
2019-10-24 11:19:40 -0400

    Add more python stuff

--- game/framework.py
+++ game/framework.py
@@ -2,9 +2,8 @@ class CVMap:
     def __init__(self):
         pass
 
-
 class Game:
-    def update_game(self, delta_t: int):
+    def update_game(self, keys, delta_t: int):
         pass
 
     def update_map(self, cv_map: CVMap):
