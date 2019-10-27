59fea573e88eb84b2825c9d6d33b95b958fc8df2
r34luo <r34luo@uwaterloo.ca>
2019-10-27 18:23:57 -0400

    Revert "bouncing (kinda) works"
    This reverts commit 71e533c79ddce2a4558a1da14d09c3bf4fa2e14f.

---
+++ game/physics/bouncephysics.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+from numpy import ndarray
+import math
+from game.physics.Vector2 import Vector2
+
+from game.physics.physics import Physics, PhysicsObject
+
+
+def force(x):
+    return x * x
+
+
+class AnglePhysics(Physics):
+    def kustify(self, obj: PhysicsObject):
+        img = obj.img(self.edges)
+        inter = cv2.bitwise_and(self.edges, img)
+        non = cv2.findNonZero(inter)
+        if non is None:
+            return
+        cnt = 0
+
+        weightedAngleSum = 0
+        weightedNum = 0
+
+        angleSum = 0
+
+        for p in non:
+            p = (p[0][0], p[0][1])
+            d = obj.distance(p)
+            a = obj.angle(p)
+            if d is not None:
+                xx, yy = d
+                # obj.x += xx
+                # obj.y += yy
+                cnt += 1
+
+                mag = math.sqrt(xx * xx + yy * yy)
+                weightedAngleSum += a * mag
+                weightedNum += mag
+
+                angleSum += a
+
+        if cnt == 0:
+            return
+
+        # angle = weightedAngleSum / weightedNum
+        angle = angleSum / cnt
+
+        velocity = Vector2(obj.vx, obj.vy)
+        vAngle = math.atan2(-obj.vy, -obj.vx)
+
+        newAngle = angle + angle - vAngle
+
+        # print(str(angle * 180 / math.pi) + " " + str(vAngle * 180 / math.pi) + " " + str(newAngle * 180 / math.pi))
+
+        obj.vx = velocity.mag() * math.cos(newAngle)
+        obj.vy = velocity.mag() * math.sin(newAngle)
