17d4d7b90a3ccf8fc6c72f82ad519753ec3c8cc4
Sunny Lan <sunny.lan@uwaterloo.ca>
2019-10-27 16:02:52 -0400

    ewrfqeffewfwef

--- game/cv.py
+++ game/cv.py
@@ -6,21 +6,21 @@ from numpy import ndarray
 
 
 class CVer:
-    lower = 100
-    higher = 200
-    thres=0.9
+    def __init__(self):
+        self.lower=100
+        self.higher=200
+        self.sobel_thres=0.8
 
     def do_cv(self, frame: ndarray) -> CVMap:
-        frame=cv2.resize(frame,(int(frame.shape[1]/2), int(frame.shape[0]/2)))
+        frame=cv2.resize(frame,(int(frame.shape[1]/1), int(frame.shape[0]/1)))
         #frame, g, r = cv2.split(frame)
-        cv2.imshow("original", frame)
 
         # convert to float image
         frame = np.float64(frame)
         frame *= 1.0 / 255
 
         # filter
-        frame = cv2.GaussianBlur(frame, (3, 3), 0)
+        frame = cv2.GaussianBlur(frame, (5, 5), 0)
 
         # take sobel x and y
         sx = cv2.Sobel(frame, cv2.CV_64F, 1, 0)
@@ -33,11 +33,11 @@ class CVer:
 
 
         # convert back
-        #res = cv2.Laplacian(frame, cv2.CV_64F)
+        # res = cv2.Laplacian(frame, cv2.CV_64F)
         b,g,r=cv2.split(res)
         res=0.11*b+ 0.59*g+ 0.30*r
-        #_,res = cv2.threshold(res, 0.01,255, cv2.THRESH_BINARY)
+        _,res = cv2.threshold(res, self.sobel_thres,255, cv2.THRESH_BINARY)
 
-        # res=cv2.Canny(frame,100,250)
+        # res=cv2.Canny(frame,70,200)
 
         return CVMap(res)
--- game/physics/physics.py
+++ game/physics/physics.py
@@ -4,6 +4,7 @@ from game.Object import GameObject
 from typing import Tuple
 import numpy as np
 from numpy import ndarray
+import cv2
 
 
 # interface for physics object
@@ -15,11 +16,19 @@ class PhysicsObject(GameObject):
     def distance(self, point: Tuple[float, float]) -> Tuple[float, float]:
         pass
 
+    def img(self, shp) -> ndarray:
+        pass
+
 
 class Circle(PhysicsObject):
     def __init__(self, x, y, r):
         super().__init__(x, y)
         self.r = r
+
+    def img(self, edg:ndarray) -> ndarray:
+        img = np.zeros(edg.shape, dtype=edg.dtype)
+        cv2.circle(img, (int(self.x), int(self.y)), self.r, 255, cv2.FILLED)
+        return img
 
     def distance(self, point: Tuple[float, float]):
         px, py = point
@@ -27,7 +36,7 @@ class Circle(PhysicsObject):
         dsqr = (dx * dx + dy * dy)
 
         if dsqr <= (self.r * self.r):
-            m = sqrt(max(dsqr,0.1))
+            m = sqrt(max(dsqr, 0.1))
             k = (self.r - m) / m
             return k * -dx, k * -dy
 
@@ -42,4 +51,3 @@ class Physics:
 
     def kustify(self, obj: PhysicsObject):
         pass
-
