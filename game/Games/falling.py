61575b9b5c246da68631ea44ab7de453dbf7ea4b
Sunny Lan <sunny.lan@uwaterloo.ca>
2019-10-31 14:43:49 -0400

    kst

--- game/Games/falling.py
+++ game/Games/falling.py
@@ -2,6 +2,7 @@ import cv2
 
 from game.framework import Game, CVMap
 from game.physics2.TempPhysics import TempPhysics
+from game.physics2.WallPhysics import WallPhysics
 from game.physics2.objects.Circle import Circle
 from game.test import test
 
@@ -15,7 +16,9 @@ class Falling(Game):
         super().__init__(mp)
         self.c = Circle(200, 10, 50)
         self.physics = TempPhysics()
+        self.wall = WallPhysics()
         self.physics.objects.append(self.c)
+        self.wall.objects.append(self.c)
 
     def update_map(self, new_map: CVMap):
         super().update_map(new_map)
@@ -25,18 +28,14 @@ class Falling(Game):
         cv2.circle(self.map.game_img, (int(c.x), int(c.y)), int(c.r), 255)
 
     def update_game(self, keys, delta_t: int):
-        # if self.map is None:
-        #     return
         c = self.c
         c.vy += ppm * G * delta_t
 
+        self.wall.update(delta_t)
         self.physics.update(delta_t)
+
         c.x += c.vx * delta_t
         c.y += c.vy * delta_t
-
-        if c.y + c.r >= self.map.edges.shape[0]:
-            c.y = self.map.edges.shape[0] - c.r
-            c.vy = 0
 
         self.draw_circle(self.c)
 
