781354b948f95dd4ba6e83d5655b968b122af57f
Sunny Lan <sunny.lan@uwaterloo.ca>
2019-10-31 11:20:23 -0400

    add game image

--- game/test.py
+++ game/test.py
@@ -8,24 +8,29 @@ from game.cv import CVer
 import numpy as np
 
 
-def test(G, still=None):
+def test(G):
     image_io = ImageIO()
 
     map_detect = CVer()
     img = image_io.get_img()
-    game = G()
+    game = G(img)
     last_time = time.time()
 
     game_img = np.zeros_like(img)
 
     while True:
         img = image_io.get_img()
+
         mp = map_detect.do_cv(img)
+        mp.game_img = game_img
+
         game.update_map(mp)
+
         t = time.time()
-        img = game.update_game([], t - last_time, game_img)
+        game.update_game([], t - last_time)
         last_time = t
-        image_io.show(img)
+
+        image_io.show(mp.game_img)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
