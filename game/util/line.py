13cf4fc669d624c320d855145c4ca3f94fc9e77a
r34luo <r34luo@uwaterloo.ca>
2019-10-31 11:27:40 -0400

    fixed distance from point to line segment

--- game/util/line.py
+++ game/util/line.py
@@ -12,18 +12,18 @@ class Line:
         # self.b = st.x - ed.x
         # self.c = -(self.a * st.x + self.b * st.y)
 
-        self.dir = self.ed - self.st
+        self.dir: Vector2 = self.ed - self.st
 
     def perp(self, pt: Vector2):
         diag = Vector2(pt.x - self.st.x, pt.y - self.st.y)
-        return diag + dir * -1 * Vector2.dot(diag, dir) / dir.mag() / dir.mag()
+        return diag - self.dir * diag.dot(self.dir) * (1 / self.dir.mag() / self.dir.mag())
 
     def distance(self, pt: Vector2):
         ret = min((self.st - pt).mag(), (self.ed - pt).mag())
 
-        perp: Vector2 = self.perp(self, pt)
+        perp: Vector2 = self.perp(pt)
         inter: Vector2 = pt - perp
-        if max((self.st - inter).mag(), (self.ed - inter).mag()) <= (self.ed - self.st).mag():
+        if max((self.st - inter).mag(), (self.ed - inter).mag()) <= self.dir.mag():
             ret = min(ret, perp.mag())
 
         return ret
