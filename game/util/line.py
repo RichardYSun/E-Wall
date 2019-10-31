d2db642abcf5a7c12144c8e07656c2eb2d7ccc72
r34luo <r34luo@uwaterloo.ca>
2019-10-31 11:41:23 -0400

    commented

--- game/util/line.py
+++ game/util/line.py
@@ -1,6 +1,8 @@
 from game.util.Vector2 import Vector2
 
 
+# class representing a line segment
+# construct with starting point st and ending point ed by calling: Line(st, ed)
 class Line:
 
     def __init__(self, st: Vector2, ed: Vector2):
@@ -14,10 +16,12 @@ class Line:
 
         self.dir: Vector2 = self.ed - self.st
 
+    # Gets the perpendicular vector from the line to a point
     def perp(self, pt: Vector2):
         diag = Vector2(pt.x - self.st.x, pt.y - self.st.y)
         return diag - self.dir * diag.dot(self.dir) * (1 / self.dir.mag() / self.dir.mag())
 
+    # gets the shortest distance from the segment to a point (note that this may not be perp distance)
     def distance(self, pt: Vector2):
         ret = min((self.st - pt).mag(), (self.ed - pt).mag())
 
