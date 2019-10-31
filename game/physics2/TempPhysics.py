89f28711fe4c419b066b6ae6ea3a892537bb2309
Sunny Lan <sunny.lan@uwaterloo.ca>
2019-10-31 10:56:27 -0400

    begin temp

--- game/physics2/TempPhysics.py
+++ game/physics2/TempPhysics.py
@@ -4,4 +4,5 @@ from game.physics2.objects.PhysicsObject import PhysicsObject
 
 class TempPhysics(MapPhysics):
     def apply_physics(self, obj: PhysicsObject, delta_t):
-        
+        for line in self.map.lines:
+            x1,y1,x2,y2,_=line
