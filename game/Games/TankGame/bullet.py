864a0dafda989bdc9bcb5ad876a0e05f6293a168
RichardYSun <richard-y-sun@hotmail.com>
2019-10-31 11:36:05 -0400

    Updated Tank Game

--- game/Games/TankGame/bullet.py
+++ game/Games/TankGame/bullet.py
@@ -1,10 +1,12 @@
 import cv2
+import imutils
 
 class Bullet:
     px, py = 0.0
     angle = 0.0
     speed = 0.5
     image = None
+    damge = 9999
 
     def __init__(self, px, py, speed, angle):
         self.px = px
@@ -16,3 +18,6 @@ class Bullet:
 
     def draw(self, img: cv2.ndarray):
         img[self.px: self.px + self.image.shape[0], self.py: self.py + self.image.shape[1]] = self.image
+
+    def bounce(self, bouncedAngle):
+        image = imutils.rotate_bound(self.image, bouncedAngle - self.angle)
