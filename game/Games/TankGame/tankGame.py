0fcacada3d997d2e737f9f0fa0556e372d99fe9a
lukeKlassen <7614lukeKlassen@users.noreply.github.com>
2019-10-31 11:36:54 -0400

    tankGame updtae

--- game/Games/TankGame/tankGame.py
+++ game/Games/TankGame/tankGame.py
@@ -1,7 +1,9 @@
 import cv2
 import math
+import imutils
 import keys as key
 from framework import Game, CVMap
+import cv2.ndarray as ndarray
 
 TurnSpeed = 0.1
 PlayerSpeed = 0.1
@@ -9,6 +11,7 @@ PlayerSpeed = 0.1
 
 class TankGame(Game):
     tanks = []
+    bullets = []
 
     def __init__(self):
         self.tanks.append(Tank(100, 100, 0, 0))
@@ -16,6 +19,7 @@ class TankGame(Game):
     def update_game(self, keys, delta_t: int):
         self.takeInput(keys)
         self.movePieces()
+        self.drawPeices(self.map.game_img)
         return self.map.edges
 
     def update_map(self, new_map: CVMap):
@@ -24,8 +28,10 @@ class TankGame(Game):
     def takeInput(self, keys: [bool]):
         if keys[key.RIGHT]:
             self.tanks[0].angle -= TurnSpeed
+            self.tanks[0].rotateRight()
         if keys[key.LEFT]:
             self.tanks[0].angle += TurnSpeed
+            self.tanks[0].rotateLeft()
         if keys[key.DOWN]:
             self.tanks[0].speed = -PlayerSpeed
         if keys[key.UP]:
@@ -36,14 +42,36 @@ class TankGame(Game):
             tank.px += tank.speed * math.cos(tank.angle)
             tank.py += tank.speed * math.sin(tank.angle)
 
+    def drawPieces(self, game_img: ndarray):
+        for tank in self.tanks:
+            tank.draw(game_img)
+        for bullet in self.bullets:
+            bullet.draw(game_img)
+
 
 class Tank:
     px, py = 0.0
     angle = 0.0  # radians
     speed = 0.0
+    sprite: cv2.ndarray
 
-    def __init__(self, px, py, speed, angle):
+    def __init__(self, px, py, speed, angle, type: int):
         self.px = px
         self.py = py
         self.angle = angle
         self.speed = speed
+        if type == 1:
+            sprite = cv2.imread('player.jpg')
+        elif type == 2:
+            sprite = cv2.imread('blue.jpg')
+        elif type == 3:
+            sprite = cv2.imread('red.jpg')
+
+    def rotateRight(self):
+        self.sprite = imutils.rotate_bound(self.image, -TurnSpeed)
+
+    def rotateLeft(self):
+        self.sprite = imutils.rotate_bound(self.image, TurnSpeed)
+
+    def draw(self, game_img: ndarray):
+        game_img[self.px: self.px + self.sprite.shape[0], self.py: self.py + self.sprite.shape[1]] = self.sprite
--- game/Games/TestGame.py
+++ game/Games/TestGame.py
@@ -1,8 +1,12 @@
-from game.framework import Game
+from game.framework import Game, CVMap
+from game.test import test
 import cv2
 
 
 class TestGame(Game):
 
     def update_game(self, keys, delta_t: int):
-        cv2.imshow('frame', self.map.edges)
+        return self.map.edges
+
+
+test(TestGame, None)
