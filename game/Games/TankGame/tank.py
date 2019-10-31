58ff751c2683273dd4a50e433b632507e8c11cc9
lukeKlassen <7614lukeKlassen@users.noreply.github.com>
2019-10-31 12:04:54 -0400

    Contains in tank

--- game/Games/TankGame/tank.py
+++ game/Games/TankGame/tank.py
@@ -1,5 +1,7 @@
 import cv2
 import imutils
+import math
+from Games.TankGame.bullet import Bullet
 
 
 class Tank:
@@ -7,7 +9,7 @@ class Tank:
     angle = 0.0  # radians
     speed = 0.0
     sprite: cv2.ndarray
-
+    sprite_radius = 32
     def __init__(self, px, py, speed, angle, type: int):
         self.px = px
         self.py = py
@@ -20,11 +22,14 @@ class Tank:
         elif type == 3:
             self.sprite = cv2.imread('red.jpg')
 
-    def rotateRight(self, TurnSpeed):
-        self.sprite = imutils.rotate_bound(self.image, -TurnSpeed)
+    def rotateRight(self, turnSpeed):
+        self.sprite = imutils.rotate_bound(self.image, -turnSpeed)
 
-    def rotateLeft(self, TurnSpeed):
-        self.sprite = imutils.rotate_bound(self.image, TurnSpeed)
+    def rotateLeft(self, turnSpeed):
+        self.sprite = imutils.rotate_bound(self.image, turnSpeed)
+
+    def contains(self, bullet: Bullet) -> bool:
+        return math.sqrt(math.pow(self.px - bullet.px, 2) + math.pow(self.py - bullet.py, 2)) < (self.sprite_radius + bullet.radius)
 
     def draw(self, game_img: cv2.ndarray):
         game_img[self.px: self.px + self.sprite.shape[0], self.py: self.py + self.sprite.shape[1]] = self.sprite
