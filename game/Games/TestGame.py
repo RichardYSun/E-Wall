be3003165777f2e5a67be57b8773b5dffa823129
lukeKlassen <7614lukeKlassen@users.noreply.github.com>
2019-10-31 10:44:53 -0400

    TANKS

--- game/Games/TestGame.py
+++ game/Games/TestGame.py
@@ -1,12 +1,8 @@
-from game.framework import Game, CVMap
-from game.test import test
+from game.framework import Game
 import cv2
 
 
 class TestGame(Game):
 
     def update_game(self, keys, delta_t: int):
-        return self.map.edges
-
-
-test(TestGame, None)
+        cv2.imshow('frame', self.map.edges)
