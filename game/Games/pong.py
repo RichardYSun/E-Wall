472ff6d33cf6a4e92a636882fd88efb1d28f1541
Sunny Lan <sunny.lan@uwaterloo.ca>
2019-11-02 19:22:01 -0400

    comment

--- game/Games/pong.py
+++ game/Games/pong.py
@@ -1,16 +1,13 @@
 import cv2
-import numpy as np
 
 from game.game import Game, GameContext
 from game.physics2.collisiontypes import COLLISION_BOUNCE
 from game.physics2.objects.circle import Circle
 from game.physics2.pixelphysics import PixelPhysics
+from game.physics2.standardphysics import StandardPhysics
 from game.physics2.wallphysics import WallPhysics
 from game.test import test
 from game.util.vector2 import Vector2
-
-G = 1
-ppm = 50
 
 
 class Pong(Game):
@@ -18,12 +15,14 @@ class Pong(Game):
     def __init__(self, mp: GameContext):
         super().__init__(mp)  # make sure to call superclass initializer
 
-        # we will use two physics in this game
+        # we will use three physics in this game: StandardPhysics, PixelPhysics, WallPhysics
+        # StandardPhysics makes objects move and applies gravity if needed
+        self.std_physics = StandardPhysics(gravity=None)  # in this game we don't want gravity
         self.pixel_physics = PixelPhysics()  # PixelPhysics handles collisions of the camera image with the game objects
         self.wall_physics = WallPhysics()  # WallPhysics makes the objects bounce off walls
         # since this is pong, we want to disable the left and right walls
-        self.wall_physics.left=False
-        self.wall_physics.right=False
+        self.wall_physics.left = False
+        self.wall_physics.right = False
 
         # we create the ball
         # Vector2(mp.width / 2, mp.height / 2) means the ball is in the center of the screen
@@ -43,26 +42,26 @@ class Pong(Game):
         # make sure to call update_map for all the physics you're using
         self.pixel_physics.update_map(new_map)
         self.wall_physics.update_map(new_map)
+        self.std_physics.update_map(new_map)
 
     def update_game(self, keys, delta_t: int):
-        # bounce off top and bottom
-        xmn, xmx, ymn, ymx = self.ball.get_bounds()
-        if xmn < 0:
-            self.ball.pos.x = self.map.width / 2
-            self.ball.vel = Vector2(200, 200)
-            self.score = (self.score[0] + 1, self.score[1])
-        if xmx > self.map.width:
-            self.ball.pos.x = self.map.width / 2
-            self.ball.vel = Vector2(200, 200)
-            self.score = (self.score[0], self.score[1] + 1)
-
         # make sure to call update for all physics the game is using
         self.pixel_physics.update(delta_t)
         self.wall_physics.update(delta_t)
+        self.std_physics.update(delta_t)
 
-        for obj in self.pixel_physics.objects:
-            obj.translate(obj.vel * delta_t)
+        # detect win conditions
+        xmn, xmx, ymn, ymx = self.ball.get_bounds()
+        if xmn < 0:  # player 1 wins
+            self.ball.pos.x = self.map.width / 2  # reset ball to center
+            self.ball.vel = Vector2(200, 200)  # reset ball velocity
+            self.score[0]+=1  # increment score
+        if xmx > self.map.width:  # player 2 wins
+            self.ball.pos.x = self.map.width / 2  # reset ball to center
+            self.ball.vel = Vector2(200, 200)  # reset ball velocity
+            self.score = (self.score[0], self.score[1] + 1)  # increment score
 
+        # as a precaution, always draw stuff at the END of the update function
         self.ball.draw_hitbox(self.map.game_img)
 
         self.draw_score()
@@ -71,10 +70,10 @@ class Pong(Game):
         font = cv2.FONT_HERSHEY_SIMPLEX
         pos = (self.map.height // 2, 200)
         colour = (0, 255, 0)
-        fontScale = 2
+        font_scale = 2
         thickness = 2
 
-        cv2.putText(self.map.game_img, str(self.score[0]) + " " + str(self.score[1]), pos, font, fontScale, colour,
+        cv2.putText(self.map.game_img, str(self.score[0]) + " " + str(self.score[1]), pos, font, font_scale, colour,
                     thickness, cv2.LINE_AA)
 
 
---
+++ game/physics2/standardphysics.py
@@ -0,0 +1,14 @@
+from game.physics2.objects.physicsobject import PhysicsObject
+from game.physics2.physics import Physics
+from game.util.vector2 import Vector2
+
+
+class StandardPhysics(Physics):
+    def __init__(self, gravity=Vector2(0, 9.81)):
+        super().__init__()
+        self.gravity = gravity
+
+    def apply_physics(self, obj: PhysicsObject, delta_t):
+        if self.gravity is None:
+            obj.vel += self.gravity * delta_t * self.map.pixels_per_meter
+        obj.pos += delta_t * obj.vel
