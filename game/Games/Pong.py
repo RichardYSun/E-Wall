608679a508229fb4820133643920f4ea92c54ad4
Sunny Lan <sunny.lan@uwaterloo.ca>
2019-11-01 21:29:44 -0400

    add bouncing

--- game/Games/Pong.py
+++ game/Games/Pong.py
@@ -11,7 +11,18 @@ from game.util.Vector2 import Vector2
 G = 1
 ppm = 50
 
-#class PongBall(Circle):
+
+class PongBall(Circle):
+    def on_collision(self, offset: Vector2):
+        self.x += offset.x
+        self.y += offset.y
+        vel = Vector2(self.vx, self.vy)
+        if vel.sq_mag()==0:
+            return
+        projed = offset.proj(vel)
+        vel = vel - projed * 2.0
+        self.vx = vel.x
+        self.vy = vel.y
 
 
 class Pong(Game):
@@ -19,8 +30,8 @@ class Pong(Game):
     def __init__(self, mp: CVMap):
         super().__init__(mp)
         self.ball = PongBall(mp.width / 2, mp.height / 2, 20)
-        self.ball.vx = 100
-        self.ball.vy = 100
+        self.ball.vx = 0
+        self.ball.vy = -200
         self.physics = PixelPhysics()
         self.physics.objects.append(self.ball)
 
@@ -29,8 +40,15 @@ class Pong(Game):
         self.physics.update_map(new_map)
 
     def update_game(self, keys, delta_t: int):
+        # bounce off top and bottom
+        xmn, xmx, ymn, ymx = self.ball.get_bounds()
+        if ymn < 0:
+            self.ball.translate(Vector2(0, -ymn + 1))
+            self.ball.vy *= -1
+        if ymn >= self.map.height:
+            self.ball.translate(Vector2(self.map.height - ymx - 1, 0))
+            self.ball.vy *= -1
 
-        # self.wall.update(delta_t)
         self.physics.update(delta_t)
 
         for obj in self.physics.objects:
@@ -39,4 +57,4 @@ class Pong(Game):
         self.ball.draw_hitbox(self.map.game_img)
 
 
-test(Pong, )
+test(Pong, None)
--- game/physics2/WallPhysics.py
+++ game/physics2/WallPhysics.py
@@ -1,24 +1,19 @@
 from game.physics2.Physics import MapPhysics
 from game.physics2.objects.Circle import Circle
 from game.physics2.objects.PhysicsObject import PhysicsObject
+from game.physics2.objects.PixelObject import PixelObject
 from game.physics2.objects.Rectangle import Rectangle
+from game.util.Vector2 import Vector2
 
 
 class WallPhysics(MapPhysics):
     def apply_physics(self, obj: PhysicsObject, delta_t):
-        if obj.obj_type == Circle.obj_type:
-            obj: Circle
-            if obj.y + obj.r >= self.map.edges.shape[0]:
-                obj.y = self.map.edges.shape[0] - obj.r
-                obj.vy = 0
+        if isinstance(obj, PixelObject):
+            xmn, xmx, ymn, ymx = obj.get_bounds()
+            if xmn<0:
+                obj.translate(Vector2(-xmn, 0))
+                obj.vx*=-1
+            if xmx>=self.map.width:
+                obj.translate(Vector2(self.map.width-xmx, 0))
+                obj.vx*=-1
 
-            if obj.y - obj.r <= 0:
-                obj.y = obj.r
-
-            if obj.x - obj.r <= 0:
-                obj.x = obj.r
-
-            if obj.x + obj.r >= self.map.edges.shape[1]:
-                obj.x = self.map.edges.shape[1] - obj.r
-        elif obj.obj_type==Rectangle.obj_type:
-            obj: Rectangle
