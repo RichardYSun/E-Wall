a6f187fb8289b089eac97c48c0c8fec86ebe1a1d
Sunny Lan <sunny.lan@uwaterloo.ca>
2019-11-14 11:52:19 -0500

    add pygame

--- game/test.py
+++ game/test.py
@@ -1,25 +1,20 @@
-import os
-
-import cv2
 import time
 
 import pygame as pygame
 
+from game.game import GameContext
 from game.imageio import ImageIO
 from game.cv import CVer
-import numpy as np
 
-from game.keys import CV_MAPPING
 from game.keys import PY_MAPPING
-from game.util import ParamWindow
 
 
-def test(G, cam='test2'):
+def test(G, cam='test2', use_pygame=True):
     image_io = ImageIO(cam)
 
     map_detect = CVer()
     img = image_io.get_img()
-    game = G(map_detect.do_cv(img))
+    game = G(map_detect.do_cv(img, GameContext(use_pygame)))
     last_time = time.time()
 
     pygame.init()
@@ -33,16 +28,10 @@ def test(G, cam='test2'):
     while True:
         img = image_io.get_img()
 
-        mp = map_detect.do_cv(img)
+        ctx = GameContext(use_pygame)
+        map_detect.do_cv(img, ctx)
 
-        show_edges = ParamWindow.get_int('show edges', 1, 1)
-        if show_edges == 1:
-            mp.game_img = mp.edges
-        else:
-            mp.game_img = np.zeros_like(mp.edges)
-        mp.game_img = mp.lsd.drawSegments(mp.game_img, mp.lines)
-
-        game.update_map(mp)
+        game.update_map(ctx)
 
         t = time.time()
         game.update_game(keys_down, t - last_time)
@@ -53,16 +42,6 @@ def test(G, cam='test2'):
             print(cnt)
             tt = 0
             cnt = 0
-
-        image_io.show(mp.game_img)
-
-        # res = cv2.waitKey(1)
-        # if res != -1:
-        #     if res in CV_MAPPING:
-        #         game.key_down(CV_MAPPING[res])
-        #         game.key_up(CV_MAPPING[res])
-        #     elif res == ord('q'):
-        #         break
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
